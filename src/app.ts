118e04f3d2f9bda03cbbfb61e2a7b6fefe43d4f8
HauPTD <haudtpse173226@fpt.edu.vn>
2025-06-02 13:01:38 +0700

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes";
+import { errorHandler } from "./middlewares/errorHandler";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use("/api", userRoutes);
+app.use(errorHandler);
+
+export default app;
