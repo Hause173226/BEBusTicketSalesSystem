33473167a50217d6b6b7ae32a53cbc901813b00c
HauPTD <haudtpse173226@fpt.edu.vn>
2025-06-03 22:37:01 +0700

    example api

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,11 +1,34 @@
 import { Request, Response } from "express";
-import { User } from "../models/User";
+import { userService } from "../services/userService";
 
 export const createUser = async (req: Request, res: Response) => {
   try {
-    const user = await User.create(req.body);
+    const user = await userService.createUser(req.body);
     res.status(201).json(user);
   } catch (err) {
     res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const getAllUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await userService.getAllUsers();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const userId = req.params.id;
+    const user = await userService.deleteUser(userId);
+    res.status(200).json(user);
+  } catch (err) {
+    if (err instanceof Error && err.message === "User not found") {
+      res.status(404).json({ error: "User not found" });
+    } else {
+      res.status(500).json({ error: "Internal Server Error" });
+    }
   }
 };
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,9 +1,12 @@
 import mongoose, { Schema } from "mongoose";
 import { IUser } from "../interfaces/User";
 
-const userSchema = new Schema<IUser>({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-});
+const userSchema = new Schema<IUser>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 export const User = mongoose.model<IUser>("User", userSchema);
