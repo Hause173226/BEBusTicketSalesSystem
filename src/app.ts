5ad1d0e8d6fecf482c9cdbc948d43eb77acca937
Mpie-T <thongtase171619@gmail.com>
2025-07-12 00:58:10 +0700

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -38,7 +38,8 @@ app.use(
   })
 );
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use("/api/users", userRoutes);
 app.use("/api/trips", tripRoutes);
 app.use("/api/route", routeRoutes);
